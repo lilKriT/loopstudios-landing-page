3c44ea2568ce29d77e78efc2965b2b7ab11b1cfb
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-31 23:39:48 +0200

    Added fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html, js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        alata: ["Alata", "sans-serif"],
+        josefin: ["Josefin Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
