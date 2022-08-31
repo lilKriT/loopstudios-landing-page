3241d1cb67d2d24b11e7153aeebb4b7a5316ded9
Piotr Filipiak <kiddhkane@gmail.com>
2022-08-31 23:41:23 +0200

    Added colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html, js}", "index.html"],
   theme: {
     extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        black: "hsl(0, 0%, 0%)",
+        darkGray: "hsl(0, 0%, 55%)",
+        veryDarkGray: "hsl(0, 0%, 41%)",
+      },
       fontFamily: {
         alata: ["Alata", "sans-serif"],
         josefin: ["Josefin Sans", "sans-serif"],
