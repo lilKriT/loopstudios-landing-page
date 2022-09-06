108ecd632ac4bd56e48266254bd5a8a5743dd7f9
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-06 23:42:17 +0200

    More styling and added basic click.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Finding Elements
 const openMobileMenuButton = document.querySelector("#open-mobile-menu");
+const closeMobileMenuButton = document.querySelector("#close-mobile-menu");
 
 // Functions
 const openMobileMenu = () => {
   console.log("Opening mobile");
 };
 openMobileMenuButton.addEventListener("click", openMobileMenu);
+
+const closeMobileMenu = () => {
+  console.log("Closing mobile");
+};
+closeMobileMenuButton.addEventListener("click", closeMobileMenu);
