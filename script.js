b79027965f136d06c5fcbeeecf9f7618a553c4a8
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-06 23:56:26 +0200

    Working on the mobile menu.

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 const openMobileMenuButton = document.querySelector("#open-mobile-menu");
 const closeMobileMenuButton = document.querySelector("#close-mobile-menu");
 
+const mobileMenu = document.querySelector("#mobile-menu");
+
 // Functions
 const openMobileMenu = () => {
-  console.log("Opening mobile");
+  //   console.log("Opening mobile");
+  mobileMenu.classList.add("mobileMenuOpen");
 };
 openMobileMenuButton.addEventListener("click", openMobileMenu);
 
 const closeMobileMenu = () => {
-  console.log("Closing mobile");
+  //   console.log("Closing mobile");
+  mobileMenu.classList.remove("mobileMenuOpen");
 };
 closeMobileMenuButton.addEventListener("click", closeMobileMenu);
